3b2451e30929b16fe1bd5392640c7797184cf8c9
Alex Quiroga <alex.c.quiroga.jaldin@gmail.com>
2023-12-23 23:52:54 +0100

    23/12/2023

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Alex Quiroga Portfolio';
+export const SITE_DESCRIPTION = 'Aerospace Engineer, Software Developer, and Data Scientist.';
