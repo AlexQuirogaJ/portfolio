f5e6ccf84af08937e98413e880e4c9e61e0eb5a9
Alex Quiroga <alex.c.quiroga.jaldin@gmail.com>
2023-12-24 15:14:14 +0100

    24/12/2023-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify'
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+
+// DEPLOYMENT CONFIGURATION
+export default defineConfig({
+    adapter: netlify(),
+    output: 'hybrid',
+  })
--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,15 @@
 
 export const SITE_TITLE = 'Alex Quiroga Portfolio';
 export const SITE_DESCRIPTION = 'Aerospace Engineer, Software Developer, and Data Scientist.';
+
+// COLORS Gray tone
+// export const PRIMARY_COLOR = '#1d2023';
+// export const SECONDARY_COLOR = '#191c1e';
+
+// COLORS blue tone
+export const PRIMARY_COLOR = '#122132';
+export const SECONDARY_COLOR = '#101627';
+
+// Color tone Test
+// export const PRIMARY_COLOR = '#bad5fd';
+// export const SECONDARY_COLOR = '#bafdbc';
