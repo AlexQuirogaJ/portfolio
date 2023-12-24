c8aa444bda4ea17619fa11d2d836f222fb511569
Alex Quiroga <alex.c.quiroga.jaldin@gmail.com>
2023-12-24 20:56:29 +0100

    24/12/2023

--- .netlify/functions-internal/ssr/chunks/generic_LBimRP9R.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_S1dxR8vu.mjs').then(n => n.g);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/prerender_QURC0Rlw.mjs
+++
@@ -1,4 +0,0 @@
-const noop = () => {};
-export const f = noop;
-export const i = noop;
-export const p = noop;
