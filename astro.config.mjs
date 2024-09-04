aa30bcc195d52f7ade494ef68f0ea9fd2c279928
Alex Quiroga <alex.c.quiroga.jaldin@gmail.com>
2024-09-04 17:59:42 +0200

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import cloudflare from "@astrojs/cloudflare";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 
 // DEPLOYMENT CONFIGURATION
+import cloudflare from '@astrojs/cloudflare';
+import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   adapter: cloudflare(),
   output: 'hybrid',
